49901840f7621d055e602dab01139bad8c074271
fjrmiao <48702502+fjrmiao@users.noreply.github.com>
2019-03-25 21:32:36 +0800

    Add files via upload
    Current progress:in inicfg.h,process data from inifile to struct

--- Source/inicfg.h
+++ Source/inicfg.h
@@ -2,27 +2,37 @@
 
 struct structSiteMap{
 	int Count;
-	LPCSTR PartA[65535];
-	LPCSTR PartB[65535];
+	LPCSTR *PartA;
+	LPCSTR *PartB;
 };
 
 struct structOptionDWORD{
 	int Count;
-	LPCSTR key[65535];
-	DWORD Value[65535];
+	LPCSTR *key;
+	DWORD *Value;
 };
 
 struct structZone{
 	int ZoneID;
-	struct structOptionDWORD OptionDW[];
-	struct structSiteMap Site[];
+	struct structOptionDWORD *OptionDW;
+	struct structSiteMap *Site; 
+	
 };
 
+struct structTempDWList{
+	DWORD dw;
+	struct structTempDWList *next;
+};
 
+struct structTempStrList{
+	DWORD dw;
+	LPCSTR str;
+	struct structTempStrList *next;
+};
 
 struct structZonecConfig{
 	int ZoneCount;
-	struct structZone Zone[];
+	struct structZone *Zone;
 };
 
 class IniConfig{
@@ -31,10 +41,76 @@ class IniConfig{
 	private:
 
 	LPCSTR lpConfigFilePath;//current config file path
+
 	
+	Str2DWList(struct structTempDWList *pDWListH,char *lpListStr){
+		char lpSpliterChar[] = "|";
+        LPCSTR lpTempSplitedChar= strtok(lpListStr,lpSpliterChar);
+        struct structTempDWList *pDWList;
+        pDWList = pDWListH;
+        pDWListH->dw =0;
+        while(lpTempSplitedChar){
+            pDWListH->dw++;
+            pDWList->next =  new struct structTempDWList();
+            pDWList=pDWList->next;
+            pDWList->dw = atoi(lpTempSplitedChar);
+            lpTempSplitedChar=strtok(NULL,lpSpliterChar);
+        }	
+		pDWList->next =NULL;	
+	}
+	
+	Str2StrList(struct structTempStrList *pStrListH,char *lpListStr){
+		char lpSpliterChar[] = "|";
+        LPCSTR lpTempSplitedChar= strtok(lpListStr,lpSpliterChar);
+        struct structTempStrList *pStrList;
+        pStrList = pStrListH;
+        pStrListH->dw =0;
+        while(lpTempSplitedChar){
+            pStrListH->dw++;
+            pStrList->next =  new struct structTempStrList();
+            pStrList=pStrList->next;
+            pStrList->str = lpTempSplitedChar;
+            lpTempSplitedChar=strtok(NULL,lpSpliterChar);
+        }	
+		pStrList->next =NULL;	
+	}
+	
+	SetZonebyDWList(struct structZonecConfig *ZoneCfg,struct structTempDWList *pDWListH){
+		struct structTempDWList *pDWList;
+		DWORD iTempCount = 0;
+		ZoneCfg->ZoneCount=pDWListH->dw;
+		ZoneCfg->Zone = new struct structZone[ZoneCfg->ZoneCount];
+		pDWList = pDWListH;
+		while(NULL != pDWList->next){
+			pDWList = pDWList->next;
+			ZoneCfg->Zone[iTempCount++].ZoneID =pDWList->dw;
+		}	
+	}
+	
+
 	Initialize(){
 		if(NULL!=lpConfigFilePath){
-			printf("DO SOMETHING ABOUT READING INI FILE");
+			
+			//read ini file common section
+			char lpZoneList[256];
+			char lpOptionList[256];
+			DWORD iZoneListSize = GetPrivateProfileString("Common","Zone","",lpZoneList,256,lpConfigFilePath);
+			DWORD iOptionListSize = GetPrivateProfileString("Common","Option","",lpOptionList,256,lpConfigFilePath);	
+			
+			//process zone key
+			struct structTempDWList pDWZoneListH;
+			Str2DWList(&pDWZoneListH,lpZoneList);
+			SetZonebyDWList(&ZoneCfg,&pDWZoneListH);
+			
+			//process option key
+			struct structTempStrList pStrOptionListH;
+			Str2StrList(&pStrOptionListH,lpOptionList);
+			//SetZonebyDWList(&ZoneCfg,&pDWZoneListH);
+			
+			printf("***DO SOMETHING ABOUT add option info to struct zonecfg***"); 
+			printf("%s,%s",lpZoneList,lpOptionList);
+			
+			system("pause");
 		}
 	}
 
